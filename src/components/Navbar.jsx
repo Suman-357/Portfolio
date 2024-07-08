ab743475b4370761e9303eb77adb80e77be0adbb
Suman-357 <4mh20cs098suman@gmail.com>
2024-07-08 11:47:28 +0530

    responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,15 @@ const Navbar = () => {
   const [menu, setMenu] = useState("home");
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const handleMenuItemClick = (menuItem) => {
+    setMenu(menuItem);
+    setIsMobileMenuOpen(false);
+  };
+
   return (
     <div className="flex items-center justify-between m-5 mx-5 md:my-5 md:mx-12">
       <div className="relative">
@@ -16,15 +25,37 @@ const Navbar = () => {
         <img src={theme_pattern} alt="" className="absolute bottom-4 right-0 -z-10 w-24" />
       </div>
       
-      <div className="md:hidden">
+      <div className="md:hidden z-20">
         <img
           src={isMobileMenuOpen ? menu_close : menu_open}
           alt="menu icon"
-          onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+          onClick={toggleMobileMenu}
+          className="cursor-pointer"
         />
       </div>
 
-      <ul className={`flex-col items-center list-none gap-6 cursor-pointer absolute top-20 left-0 right-0 bg-white md:bg-transparent md:flex md:flex-row md:relative md:top-0 md:gap-16 md:m-0 transition-transform transform md:transform-none ${isMobileMenuOpen ? 'flex' : 'hidden'}`}>
+      {/* Mobile Menu */}
+      <nav className={`md:hidden fixed top-0 right-0 h-full w-[350px] bg-[#1f0016] flex flex-col items-start gap-[30px] pt-20 pl-8 transition-transform duration-500 ease-in-out z-10 transform ${isMobileMenuOpen ? 'translate-x-0' : 'translate-x-full'}`}>
+        <AnchorLink href='#home' className="text-white" onClick={() => handleMenuItemClick('home')}>
+          Home
+          {menu === 'home' && <img src={underline} alt='' className='mt-1' />}
+        </AnchorLink>
+        <AnchorLink href='#about' className="text-white" onClick={() => handleMenuItemClick('about')}>
+          About
+          {menu === 'about' && <img src={underline} alt='' className='mt-1' />}
+        </AnchorLink>
+        <AnchorLink href='#project' className="text-white" onClick={() => handleMenuItemClick('project')}>
+          Projects
+          {menu === 'project' && <img src={underline} alt='' className='mt-1' />}
+        </AnchorLink>
+        <AnchorLink href='#contact' className="text-white" onClick={() => handleMenuItemClick('contact')}>
+          Contact
+          {menu === 'contact' && <img src={underline} alt='' className='mt-1' />}
+        </AnchorLink>
+      </nav>
+
+      {/* Desktop Menu */}
+      <ul className="hidden md:flex flex-row items-center list-none gap-16 cursor-pointer">
         <li><AnchorLink className='' href='#home'><p onClick={() => setMenu('home')}>Home</p></AnchorLink>{menu === 'home' ? <img src={underline} alt='' className='flex m-auto' /> : <></>}</li>
         <li><AnchorLink className='' offset={50} href='#about'><p onClick={() => setMenu('about')}>About</p></AnchorLink>{menu === 'about' ? <img src={underline} alt='' className='flex m-auto' /> : <></>}</li>
         <li><AnchorLink className='' offset={50} href='#project'><p onClick={() => setMenu('project')}>Projects</p></AnchorLink>{menu === 'project' ? <img src={underline} alt='' className='flex m-auto' /> : <></>}</li>
